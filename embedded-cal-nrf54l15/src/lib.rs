b8817e883156d06dbb557beb97ac7ccbbd259357
WilliamTakeshi <williamtpereira@gmail.com>
2025-12-05 14:08:28 +0100

    chore: move descriptor and descriptorchain to its own file

--- embedded-cal-nrf54l15/src/lib.rs
+++ embedded-cal-nrf54l15/src/lib.rs
@@ -1,7 +1,9 @@
 #![no_main]
 #![no_std]
+mod descriptor;
 
 use defmt_rtt as _;
+use descriptor::{Descriptor, DescriptorChain, LAST_DESC_PTR};
 use panic_probe as _;
 
 const BLOCK_SIZE: usize = 64;
@@ -90,74 +92,6 @@ impl AsRef<[u8]> for HashResult {
         }
     }
 }
-
-#[repr(C)]
-#[derive(Debug, Clone, Copy, defmt::Format)]
-pub struct Descriptor {
-    pub addr: *mut u8,
-    pub next: *mut Descriptor,
-    pub sz: u32,
-    pub dmatag: u32,
-}
-
-impl Descriptor {
-    pub fn empty() -> Self {
-        Self {
-            addr: core::ptr::null_mut(),
-            next: core::ptr::null_mut(),
-            sz: 0,
-            dmatag: 0,
-        }
-    }
-}
-
-pub struct DescriptorChain {
-    descs: [Descriptor; 4],
-    count: usize,
-}
-
-impl DescriptorChain {
-    pub fn new() -> Self {
-        Self {
-            descs: [Descriptor::empty(); 4],
-            count: 0,
-        }
-    }
-
-    pub fn push(&mut self, desc: Descriptor) {
-        assert!(self.count < 4);
-
-        let idx = self.count;
-        self.descs[idx] = desc;
-        self.count += 1;
-
-        // update links
-        if idx > 0 {
-            let prev = idx - 1;
-            self.descs[prev].next = &mut self.descs[idx];
-        }
-
-        self.descs[idx].next = LAST_DESC_PTR;
-    }
-
-    pub fn first(&mut self) -> *mut Descriptor {
-        if self.count == 0 {
-            core::ptr::null_mut()
-        } else {
-            &mut self.descs[0]
-        }
-    }
-
-    pub fn len(&self) -> usize {
-        self.count
-    }
-}
-
-#[allow(
-    clippy::manual_dangling_ptr,
-    reason = "nRF54L15 uses 1 as last-descriptor sentinel"
-)]
-const LAST_DESC_PTR: *mut Descriptor = 1 as *mut Descriptor;
 
 fn sz(n: usize) -> u32 {
     const DMA_REALIGN: usize = 0x2000_0000;
