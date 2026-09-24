403ad5d85baf388d6eca881489f66d78cd144ed4
chrysn <chrysn@fsfe.org>
2026-09-24 10:33:32 +0200

    doc: Direct link to rand
    The crates creating this link do not have the rand crate in scope, and
    can thus not link there using rustdoc links.

--- embedded-cal-libcrux/src/standalone.rs
+++ embedded-cal-libcrux/src/standalone.rs
@@ -13,7 +13,7 @@ pub type Standalone = super::Extender<StandaloneConfig>;
 
 impl Standalone {
     /// Creates a software-only cal, whose RNG functionality is backed by the system as chosen by
-    /// [`rand::make_rng()`].
+    /// [`rand::make_rng()`](https://docs.rs/rand/latest/rand/fn.make_rng.html).
     pub fn standalone() -> Self {
         Self::new(embedded_cal_rand::WithSysRng::new_from_sys(EmptyCal))
     }
--- embedded-cal-rustcrypto/src/standalone.rs
+++ embedded-cal-rustcrypto/src/standalone.rs
@@ -10,7 +10,7 @@ pub type Standalone = RustcryptoCalExtender<embedded_cal_rand::WithSysRng<empty:
 
 impl Standalone {
     /// Creates a software-only cal, whose RNG functionality is backed by the system as chosen by
-    /// [`rand::make_rng()`].
+    /// [`rand::make_rng()`](https://docs.rs/rand/latest/rand/fn.make_rng.html).
     pub fn standalone() -> Self {
         Self::new_extending(embedded_cal_rand::WithSysRng::new_from_sys(empty::EmptyCal))
     }
